d01382db69f679333c8f261a0a893d913359f8c1
Rielly Young <67346803+deepwaterpaladin@users.noreply.github.com>
2024-07-22 12:42:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name='statscanpy',
     version='0.0.1',
     description='A short description of your package',
-    long_description=open('README.md').read(encoding='utf-8'),
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown'
 )
