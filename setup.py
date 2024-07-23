378a8ec6d10406f93b249ae21fec4a691ba4de50
Rielly Young <67346803+deepwaterpaladin@users.noreply.github.com>
2024-07-23 07:30:38 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='statscanpy',
-    version='0.1.1',
-    description='Basic package for accessing StatsCan data.',
+    version='0.1.2',
+    description='Basic package for querying & downloading StatsCan data by table name.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=['pyspark','pandas']
