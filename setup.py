8d1bbdf1f4dbc789ab376e74443fb202edbdddeb
Rielly Young <rielly.young11@gmail.com>
2024-07-22 21:54:47 -0400

    updates setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='statscanpy',
-    version='0.0.3',
+    version='0.1.1',
     description='Basic package for accessing StatsCan data.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
